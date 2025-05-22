7773929173e7190a86bbd868a85a4bb4a9c7e87a
laurent <simoongalaurent427@gmail.com>
2025-05-22 20:01:58 +0200

    Updated project to toolhire and fixed render.yaml

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toolhire.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tools2go.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- toolhire/wsgi.py
+++ toolhire/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toolhire.settings')
 
+print("DJANGO_SETTINGS_MODULE =", os.environ.get('DJANGO_SETTINGS_MODULE'))
+
+
 application = get_wsgi_application()
